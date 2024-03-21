75437c6bf332925ae4387423312b91122031e2b7
Timothy Kageni <118418723+Timothyke@users.noreply.github.com>
2024-03-22 00:41:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add your login functionality here
+    // You can use JavaScript to validate inputs and make AJAX requests
+});
